2d2790e8263cd281e6007cedb83234ae0d7fd0da
QLang0224 <157908068+QLang0224@users.noreply.github.com>
2024-02-25 10:38:30 -0700

    Update server.js

--- server.js
+++ server.js
@@ -3,17 +3,13 @@ const bodyParser = require('body-parser');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-
-// Middleware to parse JSON body
 app.use(bodyParser.json());
 
-// POST route to echo back the request body
 app.post('/', (req, res) => {
     const requestBody = req.body;
     res.json(requestBody);
 });
 
-// Start the server
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
